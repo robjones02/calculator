76ee9281b81ec721f40e655c999b740e0492a085
rjones02 <rob.jones.8900@gmail.com>
2023-10-01 16:56:02 +0100

    Added further stlying and comments

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let firstNumber, secondNumber = '';
-let numberArray = [];
+let numberArray = []; //array used to store the input numbers 
 let operator = '';
 
 let textElement = document.querySelector('.js-text');
 
-function sumArray(array){
+function sumArray(array){ //returns the sum of an array
     let total = 0;
     for(let i = 0; i < array.length; i++){
         total += array[i];
@@ -12,12 +12,12 @@ function sumArray(array){
     return total - 0;
 }
 
-function calculateResult(){
+function calculateResult(){ //called when the equals button is pressed
     secondNumber = sumArray(numberArray);
     let result;
     console.log(secondNumber);
 
-    if(operator === 'addition'){
+    if(operator === 'addition'){ //calculates the result based on the operator
         result = firstNumber + secondNumber;
         textElement.innerHTML = roundDown(result);
     } else if(operator === 'multiplication'){
@@ -35,12 +35,44 @@ function calculateResult(){
     console.log(numberArray);
 }
 
-function roundDown(num){
+function roundDown(num){ //rounds down the ressult to 8 decimal places
     const decimalPlaces = 8;
     const multiplier = 10 ** decimalPlaces;
     const roundedNumber = Math.floor(num * multiplier) / multiplier;
     return parseFloat(roundedNumber.toFixed(decimalPlaces));
 }
+
+function addition(){
+    operator = 'addition';
+    firstNumber = sumArray(numberArray);
+    console.log(firstNumber);
+    numberArray.splice(0, numberArray.length);
+}
+
+function subtraction(){
+    operator = 'subtraction';
+    firstNumber = sumArray(numberArray);
+    textElement.innerHTML = firstNumber;
+    console.log(firstNumber);
+    numberArray.splice(0, numberArray.length);
+}
+
+function multiplication(){
+    operator = 'multiplication';
+    firstNumber = sumArray(numberArray);
+    textElement.innerHTML = firstNumber;
+    console.log(firstNumber);
+    numberArray.splice(0, numberArray.length);
+}
+
+function division(){
+    operator = 'division';
+    firstNumber = sumArray(numberArray);
+    textElement.innerHTML = firstNumber;
+    console.log(firstNumber);
+    numberArray.splice(0, numberArray.length);
+}
+
 
 
 
